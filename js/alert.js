67cdf215717cfe3ec2c32e25e49f72988b1186de
Darina837 <daryna.koloda@nure.ua>
2019-12-07 10:54:13 +0200

    Add new alert

--- js/alert.js
+++ js/alert.js
@@ -6,19 +6,21 @@ function CustomAlert() {
 		let dialogbox = document.getElementById('dialogbox');
 		dialogoverlay.style.display = "block";
 		dialogoverlay.style.height = winH + "px";
-		dialogbox.style.left = (winW / 2) - (550 * .5) + "px";
+		dialogbox.style.left = "33%";
 		dialogbox.style.top = "100px";
 		dialogbox.style.display = "block";
 		document.getElementById('dialogboxhead').innerHTML = `Ой... К сожалению, вы столкнулись! Ваш счёт: ${score}`;
-		document.getElementById('dialogboxbody').innerHTML = 'Хотите сыграть ещё раз?';
+		document.getElementById('dialogboxbody').innerHTML = `Хотите сыграть ещё раз?`;
 		document.getElementById('dialogboxfoot').innerHTML = '<button id="repeat" onclick="Alert.repeat()">Да, играть</button><button id="esc" onclick="Alert.esc()">Нет</button>';
 	}
 
-	this.repeat = function() {
+	this.repeat = function() { // при нажатии кнопки "ДА, ИГРАТЬ"
+		document.getElementById('gif').style.display = "none";
 		document.getElementById('dialogboxhead').innerHTML = 'Отлично!';
 		document.getElementById('dialogboxbody').innerHTML = 'Хорошей игры :)';
 		document.getElementById('dialogboxfoot').innerHTML = '<button class="ok" onclick="Alert.ok()">Ok</button>';
-		this.ok = function () {
+
+		this.ok = function () { // при нажатии кнопки "ОК", игра перезапускается
 			document.getElementById('dialogbox').style.display = "none";
 			document.getElementById('dialogoverlay').style.display = "none";
 			xPosition = 10;
@@ -35,11 +37,13 @@ function CustomAlert() {
 		}
 	}
 	
-	this.esc = function () {
+	this.esc = function () { // при нажатии кнопки "НЕТ, ВЫЙТИ"
+		document.getElementById('gif').style.display = "none";
 		document.getElementById('dialogboxhead').innerHTML = 'До свидания!';
 		document.getElementById('dialogboxbody').innerHTML = 'Хорошего времени суток :)';
 		document.getElementById('dialogboxfoot').innerHTML = '<button class="ok" onclick="Alert.ok()">Ok</button>';
-		this.ok = function () {
+
+		this.ok = function () { // при нажатии кнопки "ОК", выход в главное меню
 			document.getElementById('dialogbox').style.display = "none";
 			document.getElementById('dialogoverlay').style.display = "none";
 			return location.reload()
